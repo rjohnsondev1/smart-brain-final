7cc4d42a38bc7663d44799e773e397fda8c4c370
rjohnsondev1 <rjohnsondev1@gmail.com>
2023-11-27 23:09:32 -0500

    style

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -43,7 +43,7 @@ class Register extends React.Component {
 
   render() {
     return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <article className="br3 ba white mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
